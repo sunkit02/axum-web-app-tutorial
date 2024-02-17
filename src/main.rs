ac0008cbba7cdda3b135143be7fcbc7dc458a5c9
Sun Kit Tsui <sunkittsui@gmail.com>
2024-02-17 17:45:08 -0600

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,19 @@ use crate::web::{routes_login, routes_static};
 use axum::{middleware, Router};
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 // endregion: --- Modules
 
 #[tokio::main]
 async fn main() -> Result<()> {
+	tracing_subscriber::fmt()
+		.without_time() // For early local development
+		.with_target(false) // Set false for very simple tracing
+		.with_env_filter(EnvFilter::from_default_env())
+		.init();
+
 	// Initialize ModelManager.
 	let mm = ModelManager::new().await?;
 
@@ -39,7 +47,7 @@ async fn main() -> Result<()> {
 
 	// region:    --- Start Server
 	let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-	println!("->> {:<12} - {:?}\n", "LISTENING", listener.local_addr());
+	info!("{:<12} - {:?}\n", "LISTENING", listener.local_addr());
 	axum::serve(listener, routes_all.into_make_service())
 		.await
 		.unwrap();
